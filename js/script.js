79eb36f7d9b9f4977466af23c8964a1b445e8fb8
Michal <78083264+TenMichal@users.noreply.github.com>
2022-03-28 16:34:44 +0200

    script

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
-document.querySelectorAll('.method').forEach(method => {
+document.querySelectorAll('.delivery .method').forEach(method => {
     method.addEventListener('click', () => {
         document.querySelectorAll('.method').forEach(m => {
             m.classList.remove('active')
         })
         method.classList.add('active')
+    })
+})
+
+document.querySelectorAll('.size .part').forEach(size => {
+    size.addEventListener('click', () => {
+        document.querySelectorAll('.size .part').forEach(s => {
+            s.classList.remove('active')
+        })
+        size.classList.add('active')
     })
 })
